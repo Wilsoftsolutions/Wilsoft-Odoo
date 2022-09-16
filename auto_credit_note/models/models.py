e5e49d5f48b0f15a202b892be804d11652df4215
Wilsoftsolutions <shakeel.ahmad@wilshirelabs.com>
2022-09-16 15:02:38 +0500

    update credit note

--- auto_credit_note/models/models.py
+++ auto_credit_note/models/models.py
@@ -13,11 +13,12 @@ class InheritStockPicking(models.Model):
             lines = self.env['sale.order.line'].search([('order_id', '=', self.sale_id.id)])
             product_ids = [line.product_id.id for line in lines]
             del_carr_p_id = [rec.product_id.id for rec in self.env['delivery.carrier'].search([])]
-            ship_cost_p_id = list(set(product_ids) & set(del_carr_p_id))[0]
+            ship_cost_p_id = list(set(product_ids) & set(del_carr_p_id))
+            ship_cost_p_id = ship_cost_p_id[0] if ship_cost_p_id else ship_cost_p_id
             for rec in lines:
                 if rec.product_id.id == ship_cost_p_id:
                     cr_note.write({
-                        'invoice_line_ids': [(0,0,{
+                        'invoice_line_ids': [(0, 0, {
                             'product_id': ship_cost_p_id,
                             'price_unit': rec.price_unit,
                             'tax_ids': [(6, 0, rec.tax_id.ids)],
