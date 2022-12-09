6298487f5913a1930476b2622288c966b87f37da
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2022-12-09 12:03:22 +0500

    Update __manifest__.py

--- de_payment_taxes/__manifest__.py
+++ de_payment_taxes/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting',
-    'version': '15.0.0.1',
+    'version': '15.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base','account','account_accountant','report_xlsx'],
