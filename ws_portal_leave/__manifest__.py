d6d56d92ee3fd63e233b8f1fa8cc116add14993a
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2022-12-19 17:16:19 +0500

    updated

--- ws_portal_leave/__manifest__.py
+++ ws_portal_leave/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Leaves',
-    'version': '15.0.0.1',
+    'version': '15.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 
