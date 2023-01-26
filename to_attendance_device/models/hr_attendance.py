f55e12679dac84a231db49db8af31b56625e79c8
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2023-01-26 22:48:30 +0500

    Update hr_attendance.py

--- to_attendance_device/models/hr_attendance.py
+++ to_attendance_device/models/hr_attendance.py
@@ -71,7 +71,7 @@ class HrAttendance(models.Model):
                 for upd_att in exist_record:
                     inn_record_count+=1
                     if inn_record_count==1:
-                        
+                        leves = self.env['hr.leave']
                         upd_att.update({'attendance_status': 'late', 'company_id': attendance.employee_id.company_id.id,'att_count': att_count})
             else:
                 inn_record_count=0
