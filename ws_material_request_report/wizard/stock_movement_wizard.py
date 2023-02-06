dd987e6dd11345956bb45ff8b8a7e8fb2c53d263
ramzannoon <ramzannoon675@gmail.com>
2023-02-06 07:43:00 +0000

    update material request

--- ws_material_request_report/wizard/stock_movement_wizard.py
+++ ws_material_request_report/wizard/stock_movement_wizard.py
@@ -1,6 +1,8 @@
 # # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
+
 
 
 class StockMovementWizard(models.Model):
@@ -11,11 +13,13 @@ class StockMovementWizard(models.Model):
     date_from = fields.Datetime(string='Date From', required=True,  default=fields.date.today().replace(day=1) )
     date_to = fields.Datetime(string='Date To', required=True,  default=fields.date.today() )
     categ_id = fields.Many2one('product.category', string='Category')
-    location_ids = fields.Many2many('stock.location', string='Locations')
+    location_ids = fields.Many2many('stock.location', string='Locations', domain='[("usage","=","internal")]')
     
    
     def check_report(self):
         data = {}
+        if not self.location_ids:
+            raise UserError('Please select Location!')    
         data['form'] = self.read(['date_from','date_to','categ_id','location_ids'])[0]
         return self._print_report(data)
 
@@ -23,3 +27,4 @@ class StockMovementWizard(models.Model):
     def _print_report(self, data):
         data['form'].update(self.read(['date_from','date_to','categ_id','location_ids'])[0])
         return self.env.ref('ws_material_request_report.open_movement_report').report_action(self, data=data, config=False)
+        
